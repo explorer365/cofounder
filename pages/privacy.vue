66c2ae5fb457b41be503c8898ae0e7fb8437615b
Cross <danikay356@gmail.com>
2020-05-11 19:44:08 +0100

    change to now.json and now-latest.json

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -29,7 +29,6 @@
     <br />
     We also use Google Analytics to measure traffic to our site and how users interact with our site. The Google Analytics terms specify that no personally identifiable information may be collected through the Google Analytics software.
     <br /><br />
-    Read more about Google Analytics privacy policy <a href='https://policies.google.com/privacy' rel='noopener noreferrer' target='_blank'>here</a>.</p>
     <h2 class="subheading grey--text">How do we collect your data?</h2>
     Google Analytics sets the following cookies:
     <br />
@@ -43,21 +42,6 @@
       </li>
     </ul>
     <br />
-
-    Your web browser also automatically sends information to our hosting provider Zeit.
-    <br />
-    This can i.e. be information about wich browser and version you are using and your Internet address (IP-address). For each page that is displayed, the date and time information, which page you came from, which page you are on, etc. are also stored.
-    <br />
-    <br />
-    When you submit the test the information you provide (described above) is stored in our database.
-    <h2 class="subheading grey--text">How will we use your data?</h2>
-    We use Google Analytics to get statistics on how visitors use the site.
-    <br />
-    <br />
-    We will create statistical, aggregated data from the test results for analytical purposes.
-    <h2 class="subheading grey--text">How do we store your data?</h2>
-    <h2 class="subheading grey--text">How to contact us?</h2>
-    If you have any questions about this privacy policy, please <a href='' rel='noopener noreferrer' target='_blank'>contact</a> us.</p>
   </div>
 </template>
 
